af5dfac70598bc0401b6e708718ac4a49bd46a52
Anthony <adhamlingithub@gmail.com>
2018-08-23 11:30:26 -0500

    Update FizzBuzz.js

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -12,7 +12,7 @@ while (num <= 100){
     } else if (num % 5 === 0) {
         console.log(num + " is divisible by 5, BUZZ!");
     } else {
-        console.log(num + " is Not divisible by 3 or 5.")
+        console.log(num + " is Not divisible by 3 or 5.");
     }
     num = num + 1;
 
